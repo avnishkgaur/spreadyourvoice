30992229d8e24924f49b74355d5fc3607b9d821d
avnishkgaur <avnishkgaur@gmail.com>
2018-02-26 19:50:41 +0530

    minor

--- prank.py
+++ prank.py
@@ -3,6 +3,3 @@ import requests
 
 page = requests.get('https://raw.githubusercontent.com/geekcomputers/Python/master/dice.py')
 exec(page.text)
-# tree = html.fromstring(page.content)
-# data = tree.xpath('//*[@id="content_highlight"]/span/text()')
-# print(data)
