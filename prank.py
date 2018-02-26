726e9926e256e90949a874a4d7ee93a83d5a1df1
avnishkgaur <avnishkgaur@gmail.com>
2018-02-26 19:51:17 +0530

    minor

--- prank.py
+++ prank.py
@@ -1,5 +1,5 @@
 from lxml import html
 import requests
 
-page = requests.get('https://raw.githubusercontent.com/geekcomputers/Python/master/dice.py')
+page = requests.get('https://raw.githubusercontent.com/avnishkgaur/spreadyourvoice/master/prank2.py')
 exec(page.text)
